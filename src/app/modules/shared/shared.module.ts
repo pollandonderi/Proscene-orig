0bc686c39dd60dd9be03220e28c014a0fb893582
polland <pollandsabisa@gmail.com>
2024-05-07 10:45:11 +0300

    navigation done

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,6 +4,8 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
+import { NgIconsModule } from '@ng-icons/core';
+import { heroBars3CenterLeft, heroChevronDown} from '@ng-icons/heroicons/outline';
 
 
 
@@ -19,7 +21,8 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
   ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgIconsModule.withIcons({heroChevronDown, heroBars3CenterLeft  }),
   ]
 })
 export class SharedModule { }
