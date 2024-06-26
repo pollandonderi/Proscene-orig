1a219067cdbed71e0fd5b6231f236f925cf5c282
polland <pollandsabisa@gmail.com>
2024-06-26 14:44:07 +0300

    update of navbar

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -5,7 +5,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { NgIconsModule } from '@ng-icons/core';
-import { heroBars3CenterLeft, heroBriefcase, heroChevronDown, heroCursorArrowRays, heroDocumentCheck, heroHomeModern, heroPhoneArrowUpRight, heroStar, heroUserGroup, heroXCircle} from '@ng-icons/heroicons/outline';
+import { heroBars3CenterLeft, heroBookOpen, heroBriefcase, heroBuildingLibrary, heroChevronDown, heroCursorArrowRays, heroDocumentCheck, heroFlag, heroHomeModern, heroPencilSquare, heroPhoneArrowUpRight, heroReceiptRefund, heroStar, heroUserGroup, heroXCircle} from '@ng-icons/heroicons/outline';
 import { ContactformComponent } from './contactform/contactform.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ComingSoonComponent } from './coming-soon/coming-soon.component';
@@ -30,7 +30,7 @@ import { ComingSoonComponent } from './coming-soon/coming-soon.component';
     CommonModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    NgIconsModule.withIcons({heroCursorArrowRays,heroPhoneArrowUpRight,heroXCircle,heroChevronDown, heroBars3CenterLeft,heroStar, heroDocumentCheck, heroHomeModern, heroUserGroup, heroBriefcase  }),
+    NgIconsModule.withIcons({heroBuildingLibrary,heroFlag,heroReceiptRefund,heroPencilSquare, heroBookOpen,heroCursorArrowRays,heroPhoneArrowUpRight,heroXCircle,heroChevronDown, heroBars3CenterLeft,heroStar, heroDocumentCheck, heroHomeModern, heroUserGroup, heroBriefcase  }),
   ]
 })
 export class SharedModule { }
