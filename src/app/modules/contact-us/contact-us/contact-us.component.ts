766f06dc04743bdfc667be0d8d96eeeb729066e6
polland <pollandsabisa@gmail.com>
2024-07-02 13:20:00 +0300

    working contact form

--- src/app/modules/contact-us/contact-us/contact-us.component.ts
+++ src/app/modules/contact-us/contact-us/contact-us.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact-us',
@@ -7,29 +8,64 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent {
-  passwordForm: FormGroup = new FormGroup({
-   
-  });
+  form: FormGroup = this.fb.group({
+    message:'',
+    subject:'',
+    pnonenumber:'',
+    from_email:'',
+    to_name:'Admin',
+    from_name:'',
+    company:'',
+    jobfunction:'',
+    department:'',
+    country:''
+  })
+  async send(){
+    emailjs.init('WtTeYCIxNDYbZa2iM')
+    let response = await emailjs.send("service_vlhicni","template_307vh9s",{
+      from_name: this.form.value.from_name,
+      to_name: this.form.value.to_name,
+      from_email: this.form.value.from_email,
+      pnonenumber: this.form.value.phonenumber,
+      subject: this.form.value.subject,
+      company: this.form.value.company,
+      jobfunction: this.form.value.jobfunction,
+      department: this.form.value.department,
+      country: this.form.value.country,
+      message: this.form.value.message,
+      });
+      alert("Message has been sent");
+      this.form.reset()
+  }
     submitted = false;
     constructor(private fb: FormBuilder) {
-      this.passwordForm = fb.group({
-      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      acceptTerms: new FormControl(false, Validators.requiredTrue),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      phonenumber: new FormControl('', [Validators.required,  Validators.minLength(3)])
+      this.form = fb.group({
+        from_name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+        to_name: new FormControl(false, Validators.requiredTrue),
+        from_email: new FormControl('', [Validators.required, Validators.email]),
+      pnonenumber: new FormControl('', [Validators.required,  Validators.minLength(3)]),
+      message: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      company: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      jobfunction: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      department: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      country: new FormControl('', [Validators.required, Validators.minLength(3)]),
+        subject: new FormControl('', [Validators.required, Validators.minLength(3)]),
   
       });
     }
     get f(){
   
-      return this.passwordForm.controls;
+      return this.form.controls;
   
     }
   
     submit(){
-      if (this.passwordForm.invalid) {
+      if (this.form.invalid) {
         return;
     }
   
     }
+    
 }
+
+
