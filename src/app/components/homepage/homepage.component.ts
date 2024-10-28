ad60e2ec480ef53c6164c9414bf489e5b3b5d7e7
pollandonderi <pollandsabisa@gmail.com>
2024-10-28 08:52:52 +0300

    infinite scroll on home page

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit{
+  ngOnInit(): void {
+    const scrollers =  document.querySelectorAll(".scroller");
+if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
+  addAnimation()
+}
+
+function addAnimation(){
+  scrollers.forEach(scroller => {
+    scroller.setAttribute('data-animated', true.toString());
+    const scrollerInner = scroller.querySelector('.scroller-inner');
+    const scrollerContent = Array.from(scrollerInner!.children)
+    scrollerContent.forEach(
+      item =>{
+        const duplicatedItem = item.cloneNode(true) as Element;
+        
+        duplicatedItem.setAttribute("aria-hidden", "true")
+        scrollerInner?.appendChild(duplicatedItem)
+      }
+    )
+  })
+}
+  }
 
 }
